3a30d24b1b5d94455d3b7caf2299e1a0068a5dec
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-09-18 15:10:50 -0600

    displayed moods with button using javascript

--- scripts.js
+++ scripts.js
@@ -20,6 +20,21 @@ for (let i = 0; i < data.length; i += 1) {
     console.log(img);
     itemsContainer.appendChild(newDiv);
 
+    const desc = document.createElement ('P');
+    desc.innerText = data[i].desc;
+    newDiv.appendChild(desc);
+
+    const price = document.createElement('P');
+    price.innerText = data[i].price;
+    newDiv.appendChild(price);
+
+    //Make a button
+    const button = document.createElement('button');
+    button.id = data[i].name;
+    button.dataset.price = data[i].price;
+    button.innerHTML = "Add to Cart";
+    newDiv.appendChild(button);
+
 
 }
 
