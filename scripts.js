43b0eb171c481292f705654ef66a539814f8df91
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-09-20 23:21:35 -0600

    displayed cart items on console

--- scripts.js
+++ scripts.js
@@ -40,24 +40,77 @@ for (let i = 0; i < data.length; i += 1) {
 //using a COLLECTION (Array/Object)
 const cart = []
 
+//telling JS to produce same output as value:value (value equals value)
+//const obj = { a }
+
+
 
 //function to add to cart and keeping
 //track of name, price, and quantity using an object
 function addItemToCart(name, price) {
+    //loop through the cart to see if any names match, then update qty
+    for (let i = 0; i < cart.length; i += 1) {
+        //do comparison with if statement
+        //if the name of the object at a specific index equals name, then 
+        //update qty
+        if (cart[i].name === name) {
+            //add one to the value of qty
+            cart[i].qty += 1
+            // Stop the function in order to avoid duplicates in cart!
+            return
+        }
+    }
     //key on left, value on right
-    const item = { name: name, price: price, qty: 1 }
+    const item = { name, price, qty: 1 }
     cart.push(item)
 }
 //function to show which items are in the cart
 function showItemsInCart() {
+    const qty = getQty()
+    //to add the total sum of the cart
+   
     /* Use backquotes in order to combine variables with a 
     string of characters */
-    console.log(`You have ${cart.length} items in your cart.`)
+    console.log(`You have ${getQty()} items in your cart.`)
+    //Make a loop which will run the length of the items in the cart
+    //i+=1 means that at the end of every loop we will add 1 to i which
+    //will move on to the next index(i)
+    for (let i = 0; i < cart.length; i += 1) {
+        //print each item in the cart
+        console.log(` ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+
+    }
+}
+
+//to update total items in cart when there are duplicates..
+function getQty() {
+    let qty = 0
+    for (let i = 0; i < cart.length; i += 1) {
+        qty += cart[i].qty 
+    }
+    return qty
+
+//function with a loop to add the total sum of the cart
+//and keep a running total
+function getTotal() {
+    let total = 0
+    for (let i = 0; i < cart.length; i += 1) {
+        total += cart[i].price * cart[i].qty
+    }
+
+
+    console.log(`Total in cart: $${getTotal()}`)
+    return getTotal()
+}
 }
 
 addItemToCart('happy', 5.99)
 addItemToCart('sad', 5.99)
 addItemToCart('angry', 5.99)
+addItemToCart('happy', 5.99)
 addItemToCart('calm', 5.99)
+addItemToCart('happy', 5.99)
+addItemToCart('sad', 5.99)
+
 
 showItemsInCart()
